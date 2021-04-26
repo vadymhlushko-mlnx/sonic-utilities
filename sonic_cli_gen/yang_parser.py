4ae30c340919f83cfe245345d240b9aad9cf44a8
Vadym Hlushko <vadymh@nvidia.com>
2021-04-26 09:26:09 +0000

    _find_index_of_yang_model()
    Signed-off-by: Vadym Hlushko <vadymh@nvidia.com>

--- sonic_cli_gen/yang_parser.py
+++ sonic_cli_gen/yang_parser.py
@@ -3,23 +3,27 @@
 try:
     import os
     import sys
+    import pprint
     from config.config_mgmt import ConfigMgmt
 except ImportError as e:
     raise ImportError("%s - required module not found" % str(e))
 
+# Config DB schema view
+STATIC_TABLE = 'static'
+LIST_TABLE = 'list'
 
 class YangParser:
     """ YANG model parser """
     def __init__(self,
-                 yang_model):
-        self.yang_model = yang_model
+                 yang_model_name):
+        self.yang_model_name = yang_model_name
         self.conf_mgmt = None
 
         try:
             self.conf_mgmt = ConfigMgmt()
         except Exception as e:
             raise Exception("Failed to load the {} class".format(str(e)))
-                
+
     def fail(self, e):
         print(e)
         raise e
@@ -27,15 +31,16 @@ class YangParser:
     def yang_to_dict(self):
         yang_model_type = self._determine_yang_model_type()
 
-        if (yang_model_type == 'static'):
-            print('static')
-            pass
-        else:
-            pass
-
     def _determine_yang_model_type(self):
-        cond = True
-        if cond:
-            return 'static'
-        else:
-            return 'dynamic'
+         y_index = self._find_index_of_yang_model()
+         print("INDEX {}".format(y_index))
+
+    def _find_index_of_yang_model(self):
+        for i in range(len(self.conf_mgmt.sy.yJson)):
+            if (self.conf_mgmt.sy.yJson[i]['module']['@name'] == self.yang_model_name):
+                return i
+
+
+            
+        
+       
