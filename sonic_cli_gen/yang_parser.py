cd31f0d307b28aa8a8548a1a55d1050b8a3a6de1
Vadym Hlushko <vadymh@nvidia.com>
2021-04-26 14:05:52 +0000

    added function to init list's for module, top, tables
    Signed-off-by: Vadym Hlushko <vadymh@nvidia.com>

--- sonic_cli_gen/yang_parser.py
+++ sonic_cli_gen/yang_parser.py
@@ -18,6 +18,11 @@ class YangParser:
                  yang_model_name):
         self.yang_model_name = yang_model_name
         self.conf_mgmt = None
+        #index of yang model inside conf_mgmt.sy.yJson object
+        self.idx_yJson = None
+        self.y_module = None
+        self.y_top_level_container = None
+        self.y_tables = None
 
         try:
             self.conf_mgmt = ConfigMgmt()
@@ -28,17 +33,29 @@ class YangParser:
         print(e)
         raise e
 
-    def yang_to_dict(self):
-        yang_model_type = self._determine_yang_model_type()
+    def parse_yang_model(self):
+        self._init_yang_module_and_containers()
 
-    def _determine_yang_model_type(self):
-         y_index = self._find_index_of_yang_model()
-         print("INDEX {}".format(y_index))
+    def _determine_tables_type(self):
+        #for table in y_top_level_container['container']:
+        pass
 
+    def _init_yang_module_and_containers(self):
+        self._find_index_of_yang_model()
+
+        self.y_module = self.conf_mgmt.sy.yJson[self.idx_yJson]['module']
+        if self.y_module.get('container') is not None:
+            self.y_top_level_container = self.y_module['container']
+            self.y_tables = self.y_top_level_container['container']
+            import pdb; pdb.set_trace()
+        else:
+            raise KeyError('YANG model {} does NOT have "container" element'.format(self.yang_model_name))
+
+    # find index of yang_model inside yJson object
     def _find_index_of_yang_model(self):
         for i in range(len(self.conf_mgmt.sy.yJson)):
             if (self.conf_mgmt.sy.yJson[i]['module']['@name'] == self.yang_model_name):
-                return i
+                self.idx_yJson = i
 
 
             
