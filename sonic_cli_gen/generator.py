fa6299c55044a272a415308233b7633a88552412
Stepan Blyschak <stepanb@mellanox.com>
2021-05-12 14:46:00 +0300

    generate cli from templates
    Signed-off-by: Stepan Blyschak <stepanb@mellanox.com>

--- sonic_cli_gen/generator.py
+++ sonic_cli_gen/generator.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 
-try:
-    from sonic_cli_gen.yang_parser import YangParser
-except ImportError as e:
-    raise ImportError("%s - required module not found" % str(e))
+import jinja2
+
+from sonic_cli_gen.yang_parser import YangParser
 
 class CliGenerator:
     """ SONiC CLI generator. This class provides public API
@@ -16,23 +15,30 @@ class CliGenerator:
         """ Initialize PackageManager. """
 
         self.yang_model_name = yang_model
+        self.loader = jinja2.FileSystemLoader(['/usr/share/sonic/templates/sonic-cli-gen/'])
+        self.env = jinja2.Environment(loader=self.loader)
 
     def generate_config_plugin(self):
         """ Generate CLI plugin for 'config' CLI group. """
         parser = YangParser(self.yang_model_name)
         yang_dict = parser.parse_yang_model()
-        import pprint; pprint.pprint(yang_dict)
-        pass
+        template = self.env.get_template('config.py.j2')
+        with open('config.py', 'w') as config_py:
+            config_py.write(template.render(yang_dict))
+        
 
     def generate_show_plugin(self):
         """ Generate CLI plugin for 'show' CLI group. """
         parser = YangParser(self.yang_model_name)
         yang_dict = parser.parse_yang_model()
-        pass
+        template = self.env.get_template('show.py.j2')
+        with open('show.py', 'w') as show_py:
+            show_py.write(template.render(yang_dict))
 
     # to be implemented in the next Phases
     def generate_sonic_clear_plugin(self):
         """ Generate CLI plugin for 'sonic-clear' CLI group. """
         parser = YangParser(self.yang_model_name)
         yang_dict = parser.parse_yang_model()
-        pass
+        raise NotImplementedError
+        
