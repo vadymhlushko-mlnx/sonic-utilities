f4e83db0e07419bd8b2febf1b26f9aa00fb9f638
Vadym Hlushko <vadymh@nvidia.com>
2021-07-26 08:48:40 +0000

    Fixed UT
    Signed-off-by: Vadym Hlushko <vadymh@nvidia.com>

--- tests/rates_test.py
+++ tests/rates_test.py
@@ -11,11 +11,12 @@ sys.path.insert(0, modules_path)
 import mock_tables.dbconnector
 import show.main as show 
 
-expected_rates = """    IFACE    STATE    RX_OK        RX_BPS     RX_PPS    RX_UTIL    TX_OK       TX_BPS     TX_PPS    TX_UTIL
+expected_rates = """\
+    IFACE    STATE    RX_OK        RX_BPS     RX_PPS    RX_UTIL    TX_OK       TX_BPS     TX_PPS    TX_UTIL
 ---------  -------  -------  ------------  ---------  ---------  -------  -----------  ---------  ---------
-Ethernet0        D      N/A      0.00 B/s     0.00/s      0.00%      N/A     0.00 B/s     0.00/s      0.00%
-Ethernet4      N/A      N/A   200.00 KB/s   200.00/s      0.00%      N/A  200.05 KB/s   201.00/s      0.00%
-Ethernet8      N/A      N/A  1318.36 KB/s  9000.00/s      0.03%      N/A   12.75 MB/s  9000.00/s      0.25%
+Ethernet0        D        8      0.00 B/s     0.00/s      0.00%       10     0.00 B/s     0.00/s      0.00%
+Ethernet4      N/A        4   200.00 KB/s   200.00/s      0.00%       40  200.05 KB/s   201.00/s      0.00%
+Ethernet8      N/A        6  1318.36 KB/s  9000.00/s      0.03%       60   12.75 MB/s  9000.00/s      0.25%
 """
 
 
@@ -24,7 +25,7 @@ class TestRates(object):
     def setup_class(cls):
         print("SETUP")
         os.environ["PATH"] += os.pathsep + scripts_path
-        os.environ["UTILITIES_UNIT_TESTING"] = "1"
+        os.environ["UTILITIES_UNIT_TESTING"] = "2"
 
     def test_rates(self):
         runner = CliRunner()
