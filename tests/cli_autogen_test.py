607b254c7d631fa65a1a92acbefea06c23463a58
Vadym Hlushko <vadymh@nvidia.com>
2021-09-07 15:42:03 +0000

    Added registering of plugins to UT
    Signed-off-by: Vadym Hlushko <vadymh@nvidia.com>

--- tests/cli_autogen_test.py
+++ tests/cli_autogen_test.py
@@ -2,7 +2,12 @@ import os
 import logging
 import pprint
 
-import sonic_cli_gen.main as cli_gen
+import show.plugins as show_plugins
+
+import show.main as show
+import config.main as config
+
+from utilities_common import util_base
 
 from sonic_cli_gen.generator import CliGenerator
 from click.testing import CliRunner
@@ -13,23 +18,35 @@ logger = logging.getLogger(__name__)
 test_path = os.path.dirname(os.path.abspath(__file__))
 yang_models_path = '/usr/local/yang-models'
 
+show_device_metadata_localhost="""\
+HWSKU                   DEFAULT BGP STATUS    DOCKER ROUTING CONFIG MODE    HOSTNAME      PLATFORM                MAC                DEFAULT PFCWD STATUS    BGP ASN      DEPLOYMENT ID  TYPE       BUFFER MODEL    FRR MGMT FRAMEWORK CONFIG
+----------------------  --------------------  ----------------------------  ------------  ----------------------  -----------------  ----------------------  ---------  ---------------  ---------  --------------  ---------------------------
+Mellanox-SN3800-D112C8  down                  separated                     sonic-switch  x86_64-mlnx_msn3800-r0  1d:34:db:16:a6:00  enable                  N/A                      1  ToRRouter  N/A             N/A
+"""
+
+gen = CliGenerator(logger)
 
 class TestCliAutogen:
     @classmethod
     def setup_class(cls):
         logger.info("SETUP")
         os.environ['UTILITIES_UNIT_TESTING'] = "2"
+        gen.generate_cli_plugin('show', 'sonic-device_metadata')
+        helper = util_base.UtilHelper()
+        for plugin in helper.load_plugins(show_plugins):
+            helper.register_plugin(plugin, show.cli)
 
     @classmethod
     def teardown_class(cls):
         logger.info("TEARDOWN")
+        gen.remove_cli_plugin('show', 'sonic-device_metadata')
         os.environ['UTILITIES_UNIT_TESTING'] = "0"
 
     def test_one(self):
         runner = CliRunner()
-        db = Db()
-        obj = {'config_db': db.cfgdb}
 
-        gen = CliGenerator(logger) 
-        res = gen.generate_cli_plugin('config', 'sonic-device_metadata')
+        result = runner.invoke(show.cli.commands['device-metadata'].commands['localhost'], [])
+        logger.debug(result.output)
+        assert result.output == show_device_metadata_localhost
+
 
